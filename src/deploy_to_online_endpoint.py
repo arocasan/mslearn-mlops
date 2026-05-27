416e385bc8e44850be4e2abda676691867b9e6cb
Michel Antonio <mr@michelantonio.com>
2026-05-27 11:42:49 +0200

    Gappy testing

--- src/deploy_to_online_endpoint.py
+++ src/deploy_to_online_endpoint.py
@@ -38,7 +38,7 @@ def ensure_endpoint(ml_client: MLClient, endpoint_name: str) -> ManagedOnlineEnd
         endpoint = ml_client.online_endpoints.get(name=endpoint_name)
         return endpoint
     except Exception:
-        unique_suffix = "aroca"
+        unique_suffix = "aroc"
         name = f"{endpoint_name}-{unique_suffix}"
 
         endpoint = ManagedOnlineEndpoint(
@@ -64,7 +64,7 @@ def create_or_update_deployment(
     deployment = ManagedOnlineDeployment(
         name=deployment_name,
         endpoint_name=endpoint_name,
-        environment="AzureML-sklearn-1.0-ubuntu20.04-py38-cpu@latest",
+        environment="AzureML-sklearn-1.0-ubuntu20.04-py38-cpu:1",
         model=model,
         instance_type="Standard_D2as_v4",
         instance_count=1,
